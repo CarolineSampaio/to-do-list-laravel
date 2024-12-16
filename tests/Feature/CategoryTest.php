3e95aa0e3d3f924900cf0e6e0f8ba45b2f838e3b
Caroline Sampaio <caroline_08022@hotmail.com>
2024-12-16 11:13:38 -0300

    feat: adiciona testes atualizacao uma categoria

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -190,4 +190,137 @@ class CategoryTest extends TestCase
             'message' => 'Categoria não encontrada',
         ]);
     }
+
+    // update tests
+    public function testUserCanUpdateCategory()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $category = Category::factory()->create(['user_id' => $user->id]);
+
+        $data = ['title' => 'Updated Category'];
+        $response = $this->putJson('/api/categories/' . $category->id, $data);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['message', 'data' => ['id', 'title']])
+            ->assertJsonFragment(['title' => 'updated category']);
+    }
+
+    public function testUserCanNotUpdateCategoryWithoutAuthenticationToken()
+    {
+        $user = User::factory()->create();
+        $category = Category::factory()->create(['user_id' => $user->id]);
+
+        $data = ['title' => 'Updated Category'];
+        $response = $this->putJson('/api/categories/' . $category->id, $data);
+
+        $response->assertStatus(401)->assertJsonFragment([
+            'message' => 'Unauthenticated.',
+        ]);
+    }
+
+    public function testUserCanNotUpdateCategoryWithInvalidToken()
+    {
+        $user = User::factory()->create();
+        $category = Category::factory()->create(['user_id' => $user->id]);
+
+        $data = ['title' => 'Updated Category'];
+        $response = $this->withHeader('Authorization', 'Bearer 123')->putJson('/api/categories/' . $category->id, $data);
+
+        $response->assertStatus(401)->assertJsonFragment([
+            'message' => 'Unauthenticated.',
+        ]);
+    }
+
+    public function testUserCanNotUpdateCategoryWithoutTitle()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $category = Category::factory()->create(['user_id' => $user->id]);
+
+        $data = [];
+        $response = $this->putJson('/api/categories/' . $category->id, $data);
+
+        $response->assertStatus(422)
+            ->assertJsonStructure(['message', 'errors' => ['title']])
+            ->assertJsonFragment([
+                'message' => 'O título é obrigatório',
+            ]);
+    }
+
+    public function testUserCanNotUpdateCategoryWithEmptyTitle()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $category = Category::factory()->create(['user_id' => $user->id]);
+
+        $data = ['title' => ''];
+        $response = $this->putJson('/api/categories/' . $category->id, $data);
+
+        $response->assertStatus(422)->assertJsonStructure(['message', 'errors' => ['title']])->assertJsonFragment([
+            'message' => 'O título é obrigatório',
+        ]);
+    }
+
+    public function testUserCanNotUpdateCategoryWithInvalidStringTitle()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $category = Category::factory()->create(['user_id' => $user->id]);
+
+        $data = ['title' => 123];
+        $response = $this->putJson('/api/categories/' . $category->id, $data);
+
+        $response->assertStatus(422)->assertJsonStructure(['message', 'errors' => ['title']])->assertJsonFragment([
+            'message' => 'O título deve ser válido',
+        ]);
+    }
+
+    public function testUserCanNotUpdateCategoryWithTitleMoreThan150Characters()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $category = Category::factory()->create(['user_id' => $user->id]);
+
+        $data = ['title' => str_repeat('a', 151)];
+        $response = $this->putJson('/api/categories/' . $category->id, $data);
+
+        $response->assertStatus(422)->assertJsonStructure(['message', 'errors' => ['title']])->assertJsonFragment([
+            'message' => 'O título deve ter no máximo 150 caracteres',
+        ]);
+    }
+
+    public function testUserCanNotUpdateCategoryWithInvalidId()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $data = ['title' => 'Updated Category'];
+        $response = $this->putJson('/api/categories/123', $data);
+
+        $response->assertStatus(404)->assertJsonFragment([
+            'message' => 'Categoria não encontrada',
+        ]);
+    }
+
+    public function testUserCanNotUpdateCategoryBelongsToAnotherUser()
+    {
+        $user = User::factory()->create();
+        $category = Category::factory()->create(['user_id' => $user->id]);
+
+        $anotherUser = User::factory()->create();
+        Sanctum::actingAs($anotherUser);
+
+        $data = ['title' => 'Updated Category'];
+        $response = $this->putJson('/api/categories/' . $category->id, $data);
+
+        $response->assertStatus(404)->assertJsonFragment([
+            'message' => 'Categoria não encontrada',
+        ]);
+    }
 }
