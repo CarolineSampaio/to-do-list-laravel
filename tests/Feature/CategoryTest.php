ccf9717d0753b3146c1c1b88679bc8404ff967b6
Caroline Sampaio <caroline_08022@hotmail.com>
2024-12-16 10:32:27 -0300

    feat: adiciona testes listagem categorias
    inclui factory para simular cadastro de categorias

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
 use App\Models\User;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
@@ -86,6 +87,41 @@ class CategoryTest extends TestCase
 
         $response->assertStatus(422)->assertJsonStructure(['message', 'errors' => ['title']])->assertJsonFragment([
             'message' => 'O título deve ter no máximo 150 caracteres',
+        ]);
+    }
+
+    // list all tests
+    public function testUserCanListCategories()
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $categories = Category::factory(3)->create(['user_id' => $user->id]);
+
+        $response = $this->getJson('/api/categories');
+
+        $response->assertStatus(200)
+            ->assertJsonCount(3, 'data.*')
+            ->assertJsonStructure(
+                ['message', 'status', 'data']
+            );
+    }
+
+    public function testUserCanNotListCategoriesWithoutAuthenticationToken()
+    {
+        $response = $this->getJson('/api/categories');
+
+        $response->assertStatus(401)->assertJsonFragment([
+            'message' => 'Unauthenticated.',
+        ]);
+    }
+
+    public function testUserCanNotListCategoriesWithInvalidToken()
+    {
+        $response = $this->withHeader('Authorization', 'Bearer 123')->getJson('/api/categories');
+
+        $response->assertStatus(401)->assertJsonFragment([
+            'message' => 'Unauthenticated.',
         ]);
     }
 }
